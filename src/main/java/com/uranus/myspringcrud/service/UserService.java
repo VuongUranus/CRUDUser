d153fde45c03ebd99010537479169a168168a1a4
Doan Minh Vuong <doanminhvuong465@gmail.com>
2022-05-10 12:13:49 +0700

    Push spring boot project

---
+++ src/main/java/com/uranus/myspringcrud/service/UserService.java
@@ -0,0 +1,16 @@
+package com.uranus.myspringcrud.service;
+
+import com.uranus.myspringcrud.entity.User;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UserService {
+    List<User> getAllUser();
+
+    void saveUser(User user);
+
+    void deleteUser(Long id);
+
+    Optional<User> findUserById(Long id);
+}
